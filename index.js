819d4e6b7c608182cd80dd3416f9379e24ed9318
Amir abbas maleki <amirabbas.maleki@gmail.com>
2023-12-17 17:56:30 +0000

    Update 19 files

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-console.log("Hello CodeSandbox");
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.sendfile(__dirname + "/pages/index.html");
+});
 
+app.listen(3000, function () {
+  console.log("listening on port 3000 ....");
+});
