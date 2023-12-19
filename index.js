423989c9afbe6e71177eee784b8ca009f040ebec
Amir abbas maleki <amirabbas.maleki@gmail.com>
2023-12-19 16:11:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
   res.sendfile(__dirname + "/index.html");
 });
 
+app.get("/traineeLogin", (req, res) => {
+  res.sendfile(__dirname + "/traineeLogin.html");
+});
+
+app.get("/trainerLogin", (req, res) => {
+  res.sendfile(__dirname + "/trainerLogin.html");
+});
+
 app.listen(3000, function () {
   console.log("listening on port 3000 ....");
 });
